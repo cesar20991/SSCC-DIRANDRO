2405411de770621e5d4e52d46ed4c045c5570a76
Yoel <Yoel@192.168.1.37>
2014-06-26 10:30:17 -0500

    bien mueble

--- SSCC-DIRANDRO/src/main/java/com/sscc/service/MuebleServiceImpl.java
+++ SSCC-DIRANDRO/src/main/java/com/sscc/service/MuebleServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sscc.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sscc.form.MuebleBean;
 import com.sscc.model.Mueble;
+import com.sscc.model.Perfil;
 import com.sscc.model.Usuario;
+import com.sscc.util.DateUtil;
 
 @Service
 public class MuebleServiceImpl implements MuebleService{
@@ -20,28 +24,113 @@ public class MuebleServiceImpl implements MuebleService{
 
 	@Transactional
 	public Integer registrarMueble(MuebleBean mb, Integer IdUsuario) {
-		Usuario Usuario = new Usuario();
+		Usuario usuario = new Usuario();
+		usuario.setIdUsuario(IdUsuario);
+		
 		Mueble m = new Mueble();
-		m.setDescripcion(mb.getDescripcion());
+		m.setUsuario(usuario);
+		m.setDescripcion(get(mb.getDescripcion()));
+		m.setTipo(get(mb.getTipo()));
+		m.setNombreTipo(get(mb.getNombreTipo()));
+		m.setValor(get(mb.getValor()));
+		m.setEstado("habilitado");
+		
+		
+		DateUtil d = new DateUtil();
+		m.setFecCreacion(d.hoyTimestamp());
+		
+		em.persist(m);
+		m.setCodigo("MBL-"+m.getIdMueble());
+		
 		return m.getIdMueble();
 	}
 
 	public MuebleBean getMueble(Integer IdMueble) {
-		// TODO Auto-generated method stub
-		return null;
+		Mueble m = em.find(Mueble.class, IdMueble);
+		MuebleBean mb = new MuebleBean();
+		mb.setIdMueble(m.getIdMueble());
+		mb.setDescripcion(m.getDescripcion());
+		mb.setCodigo(m.getCodigo());
+		mb.setValor(m.getValor());
+		mb.setNombreTipo(m.getNombreTipo());
+		mb.setTipo(m.getTipo());
+		
+		Usuario u = m.getUsuario();
+		Perfil p = u.getPerfil();
+		mb.setApeMaterno(p.getApeMaterno());
+		mb.setApePaterno(p.getApePaterno());
+		mb.setSegundoNombre(p.getSegundoNombre());
+		mb.setPrimerNombre(p.getPrimerNombre());
+		mb.setCorreoElectronico(u.getCorreoElectronico());
+		mb.setTipoFiscal(p.getTipoFiscal());
+		mb.setIdPerfil(p.getIdPerfil());
+		return mb;
 	}
 
 	@Transactional
 	public MuebleBean editMueble(MuebleBean mb) {
-		// TODO Auto-generated method stub
-		return null;
+		Mueble m = em.find(Mueble.class, mb.getIdMueble());
+		m = em.merge(m);
+		
+		m.setDescripcion(get(mb.getDescripcion()));
+		m.setTipo(get(mb.getTipo()));
+		m.setNombreTipo(get(mb.getNombreTipo()));
+		m.setValor(get(mb.getValor()));
+		m.setEstado(get(mb.getEstado()));
+		
+		return mb;
 	}
 
 	public List<MuebleBean> getMueblesBean() {
-		// TODO Auto-generated method stub
-		return null;
+		List<MuebleBean> lista = new ArrayList<MuebleBean>();
+		Query qMuebles = em.createQuery("SELECT m.idMueble FROM Mueble m WHERE m.estado='habilitado'");
+		List<Integer> ids = qMuebles.getResultList();
+		for(Integer id : ids){
+			Mueble m =  em.find(Mueble.class, id);
+			MuebleBean mb = new MuebleBean();
+			mb.setIdMueble(m.getIdMueble());
+			mb.setDescripcion(m.getDescripcion());
+			mb.setCodigo(m.getCodigo());
+			mb.setValor(m.getValor());
+			mb.setNombreTipo(m.getNombreTipo());
+			mb.setTipo(m.getTipo());
+			
+			Usuario u = m.getUsuario();
+			Perfil p = u.getPerfil();
+			mb.setApeMaterno(p.getApeMaterno());
+			mb.setApePaterno(p.getApePaterno());
+			mb.setSegundoNombre(p.getSegundoNombre());
+			mb.setPrimerNombre(p.getPrimerNombre());
+			mb.setCorreoElectronico(u.getCorreoElectronico());
+			mb.setTipoFiscal(p.getTipoFiscal());
+			mb.setIdPerfil(p.getIdPerfil());
+			
+			lista.add(mb);
+		}
+		return lista;
 	}
 	
+	public Integer get(Integer valor){
+		if(valor==null){
+			return 0;
+		}
+		return valor;
+	}
 	
+
+	public Double get(Double valor){
+		if(valor==null){
+			return 0.0;
+		}
+		return valor;
+	}
+	
+
+	public String get(String valor){
+		if(valor==null){
+			return "";
+		}
+		return valor;
+	}
 	
 }
